7a0f8352fe328d89beab248d23dea04c5a0d3d8d
Candy Ho <candyho.learning@gmail.com>
2024-02-06 16:18:02 +0800

    fixed css file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Counter from "./components/Counter";
 
 function App() {
+  const [counters, setCounters] = useState([0, 0, 0]);
+
+  //Add 1 to all numbers DONE
+  const incrementAll = () => {
+    setCounters((prevArr) => {
+      return prevArr.map((item) => item + 1);
+    });
+  };
+
+  //Create a component called Counter that renders a button and a number. Increment the number by 1 if the user clicks on the button. DONE
+  const handleIncrement = (index) => {
+    console.log("clicked");
+    setCounters((prevArr) =>
+      prevArr.map((item, i) => {
+        if (i === index) return item + 1;
+        else return item;
+      })
+    );
+  };
+
+  const handleAddCounter = () => {
+    setCounters((prevArr) => [...prevArr, 0]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={incrementAll}>Add All</button>
+
+      {/* Render individual counters */}
+      {counters.map((num, index) => (
+        <Counter
+          count={num}
+          onClick={handleIncrement}
+          key={index}
+          index={index}
+        />
+      ))}
+
+      {/* Add counter button DONE*/}
+      <button onClick={handleAddCounter}>Add Counter</button>
     </div>
   );
 }
